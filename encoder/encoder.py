e356fb3627b0b9feb3e55e2079dd005e53d3100e
Tamjid Rahman <tamjidarrahman@gmail.com>
2023-10-01 17:55:57 -0400

    seaborn; fix nearest point encoder

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractproperty
 from functools import cached_property
-import numpy as np
-from geo.dists import Distribution, SphericalDistribution, HammingDistribution
+from geo.dists import Distribution, SphericalDistribution
 import logging
 
 logger = logging.getLogger()
@@ -38,25 +37,3 @@ class IdentityEncoder(Encoding):
         return self.input_distribution
 
 
-class QuadrantEncoder(Encoding):
-    """Encodes Rn into quadrants. e.g. [[4,2,-1],[0,-6.2,3]] => [[1,1,0],[1,0,1]]"""
-
-    @classmethod
-    def to_quadrant(self, x1: np.ndarray):
-        return (x1 >= 0).astype(int)
-
-    @property
-    def transformed_distribution(self) -> HammingDistribution:
-        logger.info("Encoding")
-
-        transform_distribution = HammingDistribution(
-            dim=self.input_distribution.dim,
-            num_samples=self.input_distribution.num_samples,
-            generate=False,
-        )
-        new_distribution = np.apply_along_axis(
-            self.to_quadrant, 1, self.input_distribution.distribution
-        )
-        transform_distribution.distribution = new_distribution
-
-        return transform_distribution
--- run.py
+++ run.py
@@ -1,21 +1,35 @@
 from geo.dists import SphericalDistribution, HammingDistribution
 from matplotlib import pyplot as plt
-from encoder.encoder import QuadrantEncoder
+from encoder.quadrant_encoder import QuadrantEncoder
+from encoder.nearest_point_encoder import NearestPointEncoder
 import logging
 import seaborn as sns
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+sns.set()
 
 
 logger.info("Creating Distribution")
-dist = SphericalDistribution(2, 100)
-encoder = QuadrantEncoder(dist)
+dist = SphericalDistribution(2, 1000)
+encoder = NearestPointEncoder(
+    num_points=10, neighborhood_size=3, input_distribution=dist
+)
+inputdist_df = pd.DataFrame(dist.distribution, columns=["x", "y"])
+ref_df = pd.DataFrame(encoder.reference_distribution.distribution, columns=["x", "y"])
+sns.scatterplot(inputdist_df, x="x", y="y")
+sns.scatterplot(ref_df, x="x", y="y")
+plt.show()
+
 
 logger.info("Plotting")
+
 sns.relplot(
-    x=encoder.input_distribution.distance_matrix.flatten(),
-    y=encoder.transformed_distribution.distance_matrix.flatten(),
+    # kind="line",
+    x=encoder.transformed_distribution.distance_matrix.flatten().round(1),
+    y=encoder.input_distribution.distance_matrix.flatten().round(1),
+    # errorbar=("pi", 99),
 )
 
 plt.show()
